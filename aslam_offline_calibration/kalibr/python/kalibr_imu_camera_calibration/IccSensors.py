4b25c1c29ae9847a4432673370b99dfca40cc392
JIanzhu Huai <huai.3@osu.edu>
2021-02-08 19:39:40 +0800

    keep frames, or else evaluateError for RS errors will fail

--- aslam_offline_calibration/kalibr/python/kalibr_imu_camera_calibration/IccSensors.py
+++ aslam_offline_calibration/kalibr/python/kalibr_imu_camera_calibration/IccSensors.py
@@ -77,9 +77,10 @@ class IccCamera():
             multithreading = not (showCorners or showReproj or showOneStep)
             self.targetObservations = kc.extractCornersFromDataset(self.dataset, self.detector, multithreading=multithreading)
         self.allReprojectionErrors = []
+        self.__frames = []
         #an estimate of the gravity in the world coordinate frame  
         self.gravity_w = np.array([9.80655, 0., 0.])
-        
+
     def setupCalibrationTarget(self, targetConfig, showExtraction=False, showReproj=False, imageStepping=False):
         
         #load the calibration target configuration
@@ -405,7 +406,8 @@ class IccCamera():
             frame.setGeometry(self.camera.geometry)
             initialFrameTime = acv.Time(frameTimeScalar)
             frame.setTime(initialFrameTime) # Timestamp for the frame is only used in computing covarianceMap for ReprojectionErrorAdaptiveCovariance.
-            
+            self.__frames.append(frame)
+
             #corner uncertainty
             R = np.eye(2) * self.cornerUncertainty * self.cornerUncertainty
             invR = np.linalg.inv(R)
